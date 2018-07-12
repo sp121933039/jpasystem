5de905d726b7cca2ed33d3e39e491fe3f513baef
sunpeng <splovesx@qq.com>
2018-07-12 11:49:17 +0800

    初始化执行顺序
    初始化执行顺序

--- src/main/java/com/sunpeng/jpasytem/system/initialize/initialize.java
+++ src/main/java/com/sunpeng/jpasytem/system/initialize/initialize.java
@@ -3,9 +3,11 @@ package com.sunpeng.jpasytem.system.initialize;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(1)   //多个初始化的时候执行顺序
 public class initialize implements CommandLineRunner {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
