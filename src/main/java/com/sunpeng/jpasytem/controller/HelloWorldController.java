519174372db310e6d3045c2578c4ca1051531dbc
sunpeng <splovesx@qq.com>
2018-07-12 09:48:24 +0800

    发送邮件
    发送邮件

--- src/main/java/com/sunpeng/jpasytem/controller/HelloWorldController.java
+++ src/main/java/com/sunpeng/jpasytem/controller/HelloWorldController.java
@@ -2,17 +2,24 @@ package com.sunpeng.jpasytem.controller;
 
 
 import com.sunpeng.jpasytem.entity.User;
+import com.sunpeng.jpasytem.maill.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MailService mailService;
+
     @RequestMapping("/hello")
     public String index(){
         User user = new User();
         user.setUserName("sunpeng");
         System.out.println(user.getUserName());
+//        发送邮件
+//        mailService.sendSimpleMail("********","test simple mail"," hello this is simple mail");
         return "hello world";
     }
 
---
+++ src/main/java/com/sunpeng/jpasytem/maill/MailService.java
@@ -0,0 +1,7 @@
+package com.sunpeng.jpasytem.maill;
+
+public interface MailService {
+
+
+    void sendSimpleMail(String to, String subject, String content);
+}
