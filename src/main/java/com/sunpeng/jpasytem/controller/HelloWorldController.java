cc4b6090cd5549cff977724f3c1623a81703da88
sunpeng <splovesx@qq.com>
2018-07-13 08:30:20 +0800

    test
    test

--- src/main/java/com/sunpeng/jpasytem/controller/HelloWorldController.java
+++ src/main/java/com/sunpeng/jpasytem/controller/HelloWorldController.java
@@ -3,12 +3,15 @@ package com.sunpeng.jpasytem.controller;
 
 import com.sunpeng.jpasytem.entity.User;
 import com.sunpeng.jpasytem.system.maill.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private MailService mailService;
@@ -17,9 +20,9 @@ public class HelloWorldController {
     public String index(){
         User user = new User();
         user.setUserName("sunpeng");
-        System.out.println(user.getUserName());
+        logger.info(user.getUserName());
 //        发送邮件
-        mailService.sendSimpleMail("18640580605@163.com","测试"," 测试邮件功能");
+//        mailService.sendSimpleMail("18640580605@163.com","测试"," 测试邮件功能");
         return "hello world";
     }
 
