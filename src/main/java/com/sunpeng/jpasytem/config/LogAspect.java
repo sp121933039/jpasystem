682be258ce7c0f418a8f3a7598ab0ff71ae3c75b
Unknown <splovesx@qq.com>
2018-07-17 23:32:14 +0800

    aop
    aop

--- src/main/java/com/sunpeng/jpasytem/config/LogAspect.java
+++ src/main/java/com/sunpeng/jpasytem/config/LogAspect.java
@@ -8,10 +8,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Aspect
 @Component
@@ -27,5 +24,5 @@ public class LogAspect {
 
         logger.info("Method : " +joinPoint.getSignature());
     }
-    
+
 }
