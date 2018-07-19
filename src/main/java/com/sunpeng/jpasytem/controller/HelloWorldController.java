cd67fcafbb6fea0c1fd799a56fc8b0ada584e89a
Unknown <splovesx@qq.com>
2018-07-19 22:14:04 +0800

    slf4j 改为lombok 注解
    slf4j 改为lombok 注解

--- src/main/java/com/sunpeng/jpasytem/controller/HelloWorldController.java
+++ src/main/java/com/sunpeng/jpasytem/controller/HelloWorldController.java
@@ -4,8 +4,6 @@ package com.sunpeng.jpasytem.controller;
 import com.sunpeng.jpasytem.service.IHellloService;
 import com.sunpeng.jpasytem.system.maill.MailService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/sunpeng/jpasytem/system/maill/impl/MailServiceImpl.java
+++ src/main/java/com/sunpeng/jpasytem/system/maill/impl/MailServiceImpl.java
@@ -2,8 +2,6 @@ package com.sunpeng.jpasytem.system.maill.impl;
 
 import com.sunpeng.jpasytem.system.maill.MailService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
