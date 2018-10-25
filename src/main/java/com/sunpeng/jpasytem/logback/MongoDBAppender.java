0f0c9403e4befeccd9d3224f8bf059b804937177
Unknown <splovesx@qq.com>
2018-10-25 22:06:46 +0800

    初始化logback 写入 mongodb
    初始化logback 写入 mongodb

---
+++ src/main/java/com/sunpeng/jpasytem/logback/MongoDBAppender.java
@@ -0,0 +1,29 @@
+package com.sunpeng.jpasytem.logback;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import com.mongodb.BasicDBObject;
+import com.sunpeng.jpasytem.mongodb.ApplicationContextProvider;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import javax.xml.crypto.Data;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MongoDBAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
+    @Override
+    protected void append(ILoggingEvent eventObject) {
+        MongoTemplate mongoTemplate = ApplicationContextProvider.getBean(MongoTemplate.class);
+        if (mongoTemplate != null) {
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            final BasicDBObject doc = new BasicDBObject();
+            doc.append("data", df.format(new Date()));
+            doc.append("level", eventObject.getLevel().toString());
+            doc.append("logger", eventObject.getLoggerName());
+            doc.append("thread", eventObject.getThreadName());
+            doc.append("message", eventObject.getFormattedMessage());
+            mongoTemplate.insert(doc, "log");
+
+        }
+    }
+}
